9b50ea3e5e7b214e8ad2d77d66729899da99d063
Kiike0 <enriquevilrey@gmail.com>
2024-01-29 00:23:25 +0100

    Subiendo el proyecto otra vez

--- app/src/main/java/com/example/di_appfromsoulslike/MainActivity.kt
+++ app/src/main/java/com/example/di_appfromsoulslike/MainActivity.kt
@@ -7,12 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.di_appfromsoulslike.screens.PantallaCreaciones
-import com.example.di_appfromsoulslike.screens.PantallaForo
 import com.example.di_appfromsoulslike.screens.PantallaInicial
-import com.example.di_appfromsoulslike.screens.PantallaJuegos
-import com.example.di_appfromsoulslike.screens.PantallaNoticias
 import com.example.di_appfromsoulslike.ui.theme.DI_AppFromSoulslikeTheme
 
 class MainActivity : ComponentActivity() {
